f994110a8b6a28892d6cdc058a697650891ccf1e
Tanjim Hossain <audacioustux@pop-os.localdomain>
2019-12-05 13:27:42 +0600

    2019 day2 part 2

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -1,25 +1,44 @@
-use std::error::Error;
-use std::io::{self, Read};
+use std::io::{self, Error, Read};
 
-type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
+type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut input: Vec<usize> = input
+    let input: Vec<usize> = input
         .split(",")
         .map(|s| s.parse::<usize>().unwrap())
         .collect();
-    input[1] = 12;
-    input[2] = 2;
 
-    part1(&mut input)?;
-    // part2(&input)?;
+    println!("{}", part1(&input)?);
+    println!("{}", part2(&input)?);
     Ok(())
 }
 
-fn part1(input: &mut Vec<usize>) -> Result<()> {
+fn part1(input: &Vec<usize>) -> Result<usize> {
+    Ok(intcode(&input, 49, 49)?)
+}
+
+fn part2(input: &Vec<usize>) -> Result<usize> {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            if intcode(input, noun, verb)? == 19690720 {
+                return Ok(100 * noun + verb);
+            }
+        }
+    }
+
+    Err(Error::new(
+        std::io::ErrorKind::InvalidInput,
+        "NOT POSSIBLE!",
+    ))
+}
+
+fn intcode(input: &Vec<usize>, noun: usize, verb: usize) -> Result<usize> {
+    let mut input = input.clone();
+    input[1] = noun;
+    input[2] = verb;
     let mut intcode;
     for pos in (0..input.len()).step_by(4) {
         intcode = (
@@ -31,12 +50,19 @@ fn part1(input: &mut Vec<usize>) -> Result<()> {
         match intcode.0 {
             1 => input[intcode.3] = input[intcode.1] + input[intcode.2],
             2 => input[intcode.3] = input[intcode.1] * input[intcode.2],
-            _ => println!("malformed input pos: {}, val: {}", pos, input[pos]),
+            _ => {
+                return Result::Err(Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    format!("malformed input pos: {}, val: {}", pos, input[pos]),
+                ))
+            }
         }
         if input[pos + 4] == 99 {
-            println!("{}", input[0]);
-            break;
+            return Result::Ok(input[0]);
         };
     }
-    Ok(())
+    Result::Err(Error::new(
+        std::io::ErrorKind::InvalidInput,
+        "NO HALT INSTRUCTION GIVEN!",
+    ))
 }
